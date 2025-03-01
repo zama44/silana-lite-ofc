a04b97c7f2dbbdb9d00c93f42480b0e3d9fd62be
noureddineouafy <130229691+noureddineouafy@users.noreply.github.com>
2025-03-01 01:29:26 +0000

    Update gemini.js

--- plugins/gemini.js
+++ plugins/gemini.js
@@ -1,61 +1,37 @@
-import axios from 'axios'
+import axios from "axios";
+import FormData from "form-data";
 
-let handler = async (m, {
-    command,
-    usedPrefix,
-    conn,
-    text,
-    args
-}) => {
+const gemini = {
+   chat: async (question) => {
+      let d = new FormData();
+      d.append("content", `User: ${question}`);
+      d.append("model", "@google/gemini-2.0-flash-exp");
+      
+      let head = {
+         headers: {
+            ...d.getHeaders()
+         }
+      };
+      
+      let { data: ak } = await axios.post("https://mind.hydrooo.web.id/v1/chat", d, head);
+      
+      return ak.result;
+   }
+};
 
-    if (!text) return m.reply('`يمكنك ان تبحث او تسأل اي شيء انا Gemini الذكاء الاصطناعي الخاص بغوغل إسألني ما تشاء مثال :` \n\n *.gemini من هو علي ابن طالب؟*')
-    try {
-const result = await gemini(text);
-        const {
-            key
-        } = await conn.sendMessage(m.chat, {
-            image: {
-                url: 'https://telegra.ph/file/e628941df62f8d0f8c5aa.png'
-            },
-            caption: wait
-        }, {
-            quoted: fVerif,
-            mentions: [m.sender]
-        })
-        
-
-        await conn.delay(500)
-        await conn.sendMessage(m.chat, {
-            image: {
-                url: 'https://telegra.ph/file/e628941df62f8d0f8c5aa.png'
-            },
-            caption:  `\`✨Gemini Ai ^_^ SILANA AI\`\n\n${result.reply}`,
-            edit: key
-        }, {
-            quoted: fVerif,
-            mentions: [m.sender]
-        })
-    } catch (e) {
-        throw e
-    }
+let handler = async (m, {conn, text}) => {
+   if (!text) return m.reply('Please provide a question for the chatbot.');
+   
+   try {
+      const response = await gemini.chat(text);
+      await m.reply(response);
+   } catch (error) {
+      console.error('Error fetching response from Gemini:', error);
+      await m.reply('Failed to get a response from the chatbot.');
+   }
 }
-handler.help = ["gemini"]
-handler.tags = ["ai"]
-handler.command = /^(gemini)$/i
-handler.limit= true
-handler.premium = false
 
-export default handler
-
-async function gemini(txt) {
-    try {
-        const { data  } = await axios.get(`https://hercai.onrender.com/gemini/hercai?question=${encodeURIComponent(txt)}`, {
-            headers: {
-                "content-type": "application/json",
-            },
-        })
-        return data;
-    } catch (e) {
-    console.log(e)
-}
-}
+handler.help = ['gemini']
+handler.command = ['gemini']
+handler.tags = ['ai']
+export default handler;
