b5174795597e20f8c010d9b431a7c3d09d958661
noureddineouafy <130229691+noureddineouafy@users.noreply.github.com>
2025-04-05 02:24:30 +0000

    Create emojimix.js

---
+++ plugins/emojimix.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+import { Sticker } from 'wa-sticker-formatter'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+	let [emo1, emo2] = text.split`+`
+	if (!(emo1 && emo2)) throw `Ex: *.emojimix* 😟+🥺`
+	let url = `https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${emo1}_${emo2}`
+	let res = await (await fetch(url)).json()
+	if (!res.results.length) throw 'An error occurred'
+	for (let x of res.results) {
+		let stiker = await (new Sticker(x.url, { type: 'full', categories: x.tags })).toMessage()
+		conn.sendMessage(m.chat, stiker, { quoted: m })
+	}
+}
+handler.help = ['emojimix']
+handler.tags = ['sticker']
+handler.command = /^(emojimix)$/i
+handler.limit = true
+export default handler
