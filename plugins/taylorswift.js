0fb98821bbbcaedba8b42ad9b1dbf31de19f6a9d
noureddineouafy <130229691+noureddineouafy@users.noreply.github.com>
2025-04-24 11:19:30 +0100

    Create taylorswift.js

---
+++ plugins/taylorswift.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+let handler = async (m, { conn, text, prefix, command }) => {
+    if (!text) {
+        return m.reply(`Example: ${prefix + command} Enter your text!`);
+    }
+
+    try {
+        const response = await axios.get(`https://api.agatz.xyz/api/voiceover?text=${encodeURIComponent(text)}&model=taylor_swift`);
+
+        if (response.data.status === 200) {
+            const audioUrl = response.data.data.oss_url;
+
+            // Send the audio file as a voice note
+            await conn.sendMessage(m.chat, {
+                audio: { url: audioUrl },
+                mimetype: 'audio/mpeg',
+                ptt: true // Set as a voice note
+            }, { quoted: m });
+        } else {
+            return m.reply('⚠️ An error occurred while converting text to voice.');
+        }
+    } catch (error) {
+        return m.reply('⚠️ An error occurred while contacting the API.');
+    }
+};
+
+handler.help = ['taylorswift'];
+handler.tags = ['ai'];
+handler.command = ['taylorswift'];
+handler.limit = true 
+export default handler;
